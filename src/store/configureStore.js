e11c28ac11434771ab4dac79b680298c969cc590
root <root@JONNYSTATION.localdomain>
2018-01-16 23:22:04 -0600

    removed silly console logs

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  console.log('PRODUCTION ENV')
   module.exports = require('./configureStore.prod')
 } else {
-  console.log('DEVELOPMENT ENV')
   module.exports = require('./configureStore.dev')
 }
