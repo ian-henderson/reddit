e6078811a7e6f1eec7c7434fddf603ba6c3afbaa
root <root@JONNYSTATION.localdomain>
2018-01-16 22:37:05 -0600

    I am not happy with this.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
+module.exports = require('./configureStore.dev')
+/*
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
 } else {
   console.log('In Development Mode')
   module.exports = require('./configureStore.dev')
 }
+*/
